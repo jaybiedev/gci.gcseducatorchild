2cba84e626fc5f9a8b57a507c1a00a55c4f7edcc
jaybie <jaybiedev@gmail.com>
2020-07-02 12:48:00 -0500

    tabs

--- youareincluded.php
+++ youareincluded.php
@@ -22,7 +22,38 @@ if (have_posts()) : while (have_posts()) : the_post();
         <?php do_action('educator_edge_after_container_open'); ?>
         
         <div class="<?php echo esc_attr($edgt_holder_params['inner']); ?>">
-            <?php educator_edge_get_blog_single('standard'); ?>
+            <div id="tabs">
+                <ul>
+                    <li><a href="#tabs-video">Video</a></li>
+                    <li><a href="#tabs-audio">Audio</a></li>
+                    <li><a href="#tabs-download">Download</a></li>
+                    <li><a href="#tabs-trancript">Transcript</a></li>
+                </ul>
+                <div id="tabs-video">
+                    <p>
+                        <video controls="controls" width="80%" height="auto">
+                            <source src="http://gcitv.net/dl/YI/YI008-320.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </p>
+                </div>
+                <div id="tabs-audio">
+                    <p>
+                        <audio controls="controls">
+                            <source src="http://gcitv.net/dl/YI/YI008.mp3" type="audio/mpeg" width="100%" />
+                            Your browser does not support online audio player.
+                        </audio>
+                    </p>
+                </div>
+                <div id="tabs-download">
+                    <p>Link1</p>
+                    <p>Link2</p>
+                </div>
+                <div id="tabs-trancript">
+                    <?php educator_edge_get_blog_single('standard'); ?>
+                </div>                
+            </div>
+            
         </div>
         
         <?php do_action('educator_edge_before_container_close'); ?>
