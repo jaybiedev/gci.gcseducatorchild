1c7693a003b985414836111efbac213b220c2901
jaybie <jaybiedev@gmail.com>
2020-07-02 13:51:44 -0500

    tabs

--- youareincluded.php
+++ youareincluded.php
@@ -60,10 +60,11 @@ if (have_posts()) : while (have_posts()) : the_post();
 <?php endwhile; endif;
 
 get_footer(); ?>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script>
-    jQuery(document).ready(function() {
+jQuery(document).ready(function() {
     jQuery( function() {
         jQuery( "#yi-tabs" ).tabs();
     });
 })
-    </script>
+</script>
