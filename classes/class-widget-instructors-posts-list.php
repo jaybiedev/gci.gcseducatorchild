637399ee04963b72fd40f2e586d08612520cc071
jaybie <jaybiedev@gmail.com>
2020-07-04 11:02:15 -0500

    cats

--- classes/class-widget-instructors-posts-list.php
+++ classes/class-widget-instructors-posts-list.php
@@ -165,7 +165,7 @@ class WP_Widget_Instructors_Posts_List extends WP_Widget {
 		<p>
 			<label for="<?php echo esc_attr( $this->get_field_id( 'sortby' ) ); ?>"><?php _e( 'Sort by:' ); ?></label>
 			<select name="<?php echo esc_attr( $this->get_field_name( 'sortby' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'sortby' ) ); ?>" class="widefat">
-					<option value="post_title"<?php selected( $instance['sortby'], 'post_title' ); ?>><?php _e( 'Instructor Name' ); ?></option>
+					<option value="post_title"<?php selected( $instance['sortby'], 'post_title' ); ?>><?php _e( 'Name' ); ?></option>
 					<option value="menu_order"<?php selected( $instance['sortby'], 'menu_order' ); ?>><?php _e( 'Order' ); ?></option>
 					<option value="ID"<?php selected( $instance['sortby'], 'ID' ); ?>><?php _e( 'Page ID' ); ?></option>
 			</select>
@@ -178,7 +178,7 @@ class WP_Widget_Instructors_Posts_List extends WP_Widget {
 				);
 				$terms = get_terms( $args );
 				
-				//print_r($terms);
+				print_r($terms);
 				foreach( $terms as $id => $name ) { 
 					$checked = "";
 					if(in_array($name->term_id, $instance['includeCats'])){
