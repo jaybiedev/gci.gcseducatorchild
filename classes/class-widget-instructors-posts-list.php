3e0258e169631319e4474af777191393870b6130
jaybie <jaybiedev@gmail.com>
2020-07-04 16:09:18 -0500

    yi

--- classes/class-widget-instructors-posts-list.php
+++ classes/class-widget-instructors-posts-list.php
@@ -80,7 +80,8 @@ class WP_Widget_Instructors_Posts_List extends WP_Widget {
 					'no_found_rows'       => true,
 					'post_status'         => 'publish',
 					'ignore_sticky_posts' => true,
-					'orderby'			  => 'title',
+					'orderby'             => 'title',
+					'order'               => 'ASC',
 					'tax_query' => array(
 						'relation' => 'AND',
 							array(
